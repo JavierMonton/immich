e398650e83c7ca547954f611c15d0ab9b6a57559
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-09-04 07:45:35 -0400

    feat(web): add skip link to sidebar

--- web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
+++ web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import SkipLink from '$lib/components/elements/buttons/skip-link.svelte';
+  import { headerId } from '$lib/components/layouts/user-page-layout.svelte';
 </script>
 
 <section
@@ -6,5 +8,6 @@
   tabindex="-1"
   class="immich-scrollbar group relative z-10 flex w-18 flex-col gap-1 overflow-y-auto bg-immich-bg pt-8 transition-all duration-200 dark:bg-immich-dark-bg hover:sm:w-64 hover:sm:border-r hover:sm:pr-6 hover:sm:shadow-2xl hover:sm:dark:border-r-immich-dark-gray md:w-64 md:pr-6 hover:md:border-none hover:md:shadow-none"
 >
+  <SkipLink target={`#${headerId}`}>Skip to page content</SkipLink>
   <slot />
 </section>
